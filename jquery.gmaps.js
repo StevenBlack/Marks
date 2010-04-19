02000591905ce6d8c2489be1d021ca9330afc125
Steven Black <steveb@stevenblack.com>
2010-04-18 20:48:56 -0400

    opts now called settings

--- jquery.gmaps.js
+++ jquery.gmaps.js
@@ -16,13 +16,13 @@
 (function($)
 {
 	// Main plugin function
-	$.fn.gMap = function(options)
+	$.fn.gMap = function( options )
 	{
 		// Check if the browser is compatible with Google Maps
 		if ( !window.GBrowserIsCompatible || !GBrowserIsCompatible() ) { return this };
 		
 		// Build main options before element iteration
-		var opts = $.extend( {}, $.fn.gMap.defaults, options );
+		var settings = $.extend( {}, $.fn.gMap.defaults, options );
     
 		var gmarkers= $.fn.gMap.gMarkers;
 		
@@ -39,30 +39,30 @@
 			gmarkers= $gmap.gMarkers;
 			
 			// Try to center to the first marker
-			if (!opts.latitude && !opts.longitude)
+			if (!settings.latitude && !settings.longitude)
 			{
 				// Check for at least one marker
-				if ($.isArray(opts.markers) && opts.markers.length >= 1)
+				if ( $.isArray( settings.markers) && settings.markers.length >= 1)
 				{
 					// Center to the first marker
-					opts.latitude  =  opts.markers[0].latitude;
-					opts.longitude =  opts.markers[0].longitude;
+					settings.latitude  =  settings.markers[ 0 ].latitude;
+					settings.longitude =  settings.markers[ 0 ].longitude;
 				}
 				else
 				{
 					// Center Earth and lower zoom
-					opts.latitude = 34.885931;
-					opts.longitude = 9.84375;
-					opts.zoom = 2;
+					settings.latitude = 34.885931;
+					settings.longitude = 9.84375;
+					settings.zoom = 2;
 				}
 			}
 			
 			// Center the map and set the maptype
-			$gmap.setCenter(new GLatLng(opts.latitude, opts.longitude), opts.zoom);
-			$gmap.setMapType(opts.maptype);
+			$gmap.setCenter( new GLatLng( settings.latitude, settings.longitude ), settings.zoom );
+			$gmap.setMapType( settings.maptype );
 			
 			// Check for custom map controls
-			if (opts.controls.length === 0)
+			if ( settings.controls.length === 0 )
 			{
 				// Default map controls
 				$gmap.setUIToDefault();
@@ -70,42 +70,42 @@
 			else
 			{
 				// Add custom map controls
-				for (var i = 0; i < opts.controls.length; i++)
+				for (var i = 0; i < settings.controls.length; i++)
 				{
 					// Eval is evil - I know. ;)
-					eval('$gmap.addControl(new ' + opts.controls[i] + '());');
+					eval( '$gmap.addControl( new ' + settings.controls[i] + '());' );
 				}
 			}
 						
 			// Check if scrollwheel should be enabled when using custom controls
-			if (opts.scrollwheel === true && opts.controls.length !== 0) { $gmap.enableScrollWheelZoom(); }
+			if ( settings.scrollwheel === true && settings.controls.length !== 0 ) { $gmap.enableScrollWheelZoom(); }
 									
 			// Add all map markers
-			for (var j = 0; j < opts.markers.length; j++)
+			for ( var j = 0; j < settings.markers.length; j++ )
 			{
 				// Get the options from current marker
-				marker = opts.markers[j];
+				marker = settings.markers[j];
 				
 				// Create new icon
 				gicon = new GIcon();
 				
 				// Set icon properties from global options
-				gicon.image = opts.icon.image;
-				gicon.shadow = opts.icon.shadow;
-				gicon.iconSize = ($.isArray(opts.icon.iconsize)) ? new GSize(opts.icon.iconsize[0], opts.icon.iconsize[1]) : opts.icon.iconsize;
-				gicon.shadowSize = ($.isArray(opts.icon.shadowsize)) ? new GSize(opts.icon.shadowsize[0], opts.icon.shadowsize[1]) : opts.icon.shadowsize;
-				gicon.iconAnchor = ($.isArray(opts.icon.iconanchor)) ? new GPoint(opts.icon.iconanchor[0], opts.icon.iconanchor[1]) : opts.icon.iconanchor;
-				gicon.infoWindowAnchor = ($.isArray(opts.icon.infowindowanchor)) ? new GPoint(opts.icon.infowindowanchor[0], opts.icon.infowindowanchor[1]) : opts.icon.infowindowanchor;
+				gicon.image = settings.icon.image;
+				gicon.shadow = settings.icon.shadow;
+				gicon.iconSize = ( $.isArray( settings.icon.iconsize) ) ? new GSize( settings.icon.iconsize[ 0 ], settings.icon.iconsize[ 1 ] ) : settings.icon.iconsize;
+				gicon.shadowSize = ( $.isArray( settings.icon.shadowsize ) ) ? new GSize( settings.icon.shadowsize[ 0 ], settings.icon.shadowsize[ 1 ] ) : settings.icon.shadowsize;
+				gicon.iconAnchor = ( $.isArray( settings.icon.iconanchor ) ) ? new GPoint( settings.icon.iconanchor[ 0 ], settings.icon.iconanchor[ 1 ] ) : settings.icon.iconanchor;
+				gicon.infoWindowAnchor = ( $.isArray( settings.icon.infowindowanchor ) ) ? new GPoint( settings.icon.infowindowanchor[ 0 ], settings.icon.infowindowanchor[ 1 ] ) : settings.icon.infowindowanchor;
 				
 				if (marker.icon)
 				{
 					// Overwrite global options with ther marker one's
 					gicon.image = marker.icon.image;
 					if ( marker.icon.shadow ) { gicon.shadow = marker.icon.shadow; }
-					if ( marker.icon.iconsize ) { gicon.iconSize = ($.isArray(marker.icon.iconsize)) ? new GSize(marker.icon.iconsize[0], marker.icon.iconsize[1]) : marker.icon.iconsize; }
-					if ( marker.icon.shadowsize ) { gicon.shadowSize = ($.isArray(marker.icon.shadowsize)) ? new GSize(marker.icon.shadowsize[0], marker.icon.shadowsize[1]) : marker.icon.shadowsize; }
-					if ( marker.icon.iconanchor ) { gicon.iconAnchor = ($.isArray(marker.icon.iconanchor)) ? new GPoint(marker.icon.iconanchor[0], marker.icon.iconanchor[1]) : marker.icon.iconanchor; }
-					if ( marker.icon.infowindowanchor ) { gicon.infoWindowAnchor = ($.isArray(marker.icon.infowindowanchor)) ? new GPoint(marker.icon.infowindowanchor[0], marker.icon.infowindowanchor[1]) : marker.icon.infowindowanchor; }
+					if ( marker.icon.iconsize ) { gicon.iconSize = ( $.isArray( marker.icon.iconsize ) ) ? new GSize( marker.icon.iconsize[ 0 ], marker.icon.iconsize[ 1 ]) : marker.icon.iconsize; }
+					if ( marker.icon.shadowsize ) { gicon.shadowSize = ( $.isArray( marker.icon.shadowsize ) ) ? new GSize( marker.icon.shadowsize[ 0 ], marker.icon.shadowsize[ 1 ]) : marker.icon.shadowsize; }
+					if ( marker.icon.iconanchor ) { gicon.iconAnchor = ( $.isArray( marker.icon.iconanchor ) ) ? new GPoint( marker.icon.iconanchor[ 0 ], marker.icon.iconanchor[ 1 ]) : marker.icon.iconanchor; }
+					if ( marker.icon.infowindowanchor ) { gicon.infoWindowAnchor = ( $.isArray( marker.icon.infowindowanchor ) ) ? new GPoint( marker.icon.infowindowanchor[ 0 ], marker.icon.infowindowanchor[ 1 ] ) : marker.icon.infowindowanchor; }
 				}
 							
 				var gmarkerOptions= { icon: gicon };
@@ -124,14 +124,14 @@
 				if ( marker.html )
 				{
 					// Bind the info window to marker
-					gmarker.bindInfoWindowHtml(opts.html_prepend + marker.html + opts.html_append);
+					gmarker.bindInfoWindowHtml(settings.html_prepend + marker.html + settings.html_append);
 					// Add overlay if marker was created and check if popup should be shown when map is loaded
 					if ( gmarker ) { $gmap.addOverlay(gmarker); }
-					if ( marker.popup === true ) { gmarker.openInfoWindowHtml( opts.html_prepend + marker.html + opts.html_append ); }
+					if ( marker.popup === true ) { gmarker.openInfoWindowHtml( settings.html_prepend + marker.html + settings.html_append ); }
 				} else
 				{
 					// Add overlay marker
-					if (gmarker) { $gmap.addOverlay(gmarker); }
+					if (gmarker) { $gmap.addOverlay( gmarker ); }
 				}
 			}
 		});
